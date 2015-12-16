87f7cb0f44b4911e017b341841894fd732f0f022
laurii <lauri@Lauris-MBP.lan>
2015-12-16 23:55:46 +0200

    added downloading data

--- R/download_data.R
+++ R/download_data.R
@@ -4,9 +4,11 @@
 #' @description Opendata.ee riigi kinnisvaraobjektide nimekiri
 data_fin_riigikinnisvara_objektid <- function() {
     require(RCurl)
+    print("downloading data...")
     csv <- getURL("https://opendata.riik.ee/dataset/df7cd482-0391-4231-b67f-1efd7c4e8a43/resource/8121cbff-f1ff-4d09-954a-aac829a55d32/download/varadeloend.csv")
     df <- read.csv(textConnection(csv),sep = ";")
-  return(df)
+    print("downloading data...")
+    return(df)
   }
 
 #' Stuktuuritoetused
@@ -15,8 +17,10 @@ data_fin_riigikinnisvara_objektid <- function() {
 #' @description Opendata.ee riigi struktuuritoetuste nimekiri
 data_fin_struktuuritoetus <- function() {
     require(RCurl)
+    print("downloading data...")
     csv <- getURL("https://opendata.riik.ee/dataset/0b8b73d3-ba64-482c-a873-a0b89e745863/resource/61b188b8-eacc-4ce2-9a53-72b7320be4eb/download/toetusesaajad80715.csv")
     df <- read.csv(textConnection(csv),sep = ";")
+    print("downloading data...")
     return(df)
 }
 
@@ -26,8 +30,10 @@ data_fin_struktuuritoetus <- function() {
 #' @description Opendata.ee riigi riigihangete registri info
 data_fin_riigihangete_register <- function() {
     require(RCurl)
+    print("downloading data...")
     csv <- getURL("https://opendata.riik.ee/dataset/2681ade3-5250-4283-8f9c-16633a4b081f/resource/f104acb4-736a-499f-8bda-609c5439479d/download/rhravaandmed.csv")
     df <- read.csv(textConnection(csv),sep = ";")
+    print("downloading done")
     return(df)
 }
 
