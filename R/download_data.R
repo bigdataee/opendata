c0a00c9e5bdab9e9a5bf428fcf83e151ed7117c8
taivop <taivo.pungas@gmail.com>
2016-02-19 16:59:21 +0100

    improve download progress notification

--- R/download_data.R
+++ R/download_data.R
@@ -4,10 +4,10 @@
 #' @description Opendata.ee riigi kinnisvaraobjektide nimekiri
 data_fin_riigikinnisvara_objektid <- function() {
     require(RCurl)
-    print("downloading data...")
+    cat("Downloading data... ")
     csv <- getURL("https://opendata.riik.ee/dataset/df7cd482-0391-4231-b67f-1efd7c4e8a43/resource/8121cbff-f1ff-4d09-954a-aac829a55d32/download/varadeloend.csv")
     df <- read.csv(textConnection(csv),sep = ";")
-    print("downloading data...")
+    cat("done.\n")
     return(df)
   }
 
@@ -17,10 +17,10 @@ data_fin_riigikinnisvara_objektid <- function() {
 #' @description Opendata.ee riigi struktuuritoetuste nimekiri
 data_fin_struktuuritoetus <- function() {
     require(RCurl)
-    print("downloading data...")
+    cat("Downloading data... ")
     csv <- getURL("https://opendata.riik.ee/dataset/0b8b73d3-ba64-482c-a873-a0b89e745863/resource/61b188b8-eacc-4ce2-9a53-72b7320be4eb/download/toetusesaajad80715.csv")
     df <- read.csv(textConnection(csv),sep = ";")
-    print("downloading data...")
+    cat("done.\n")
     return(df)
 }
 
@@ -31,10 +31,10 @@ data_fin_struktuuritoetus <- function() {
 #' @param none
 data_fin_riigihangete_register <- function() {
     require(RCurl)
-    print("downloading data...")
+    cat("Downloading data... ")
     csv <- getURL("https://opendata.riik.ee/dataset/2681ade3-5250-4283-8f9c-16633a4b081f/resource/f104acb4-736a-499f-8bda-609c5439479d/download/rhravaandmed.csv")
     df <- read.csv(textConnection(csv),sep = ";")
-    print("downloading done")
+    cat("done.\n")
     return(df)
 }
 
@@ -56,11 +56,11 @@ data_riigiteenused <- function(){
 #' @param none
 data_ppa_piiripunktid <- function() {
   require(RCurl)
-  print("Downloading data...")
+  cat("Downloading data... ")
   csv_text <- getURL("https://www.politsei.ee/dotAsset/528868.csv")
   csv_text <- gsub("\u0096", "-", csv_text) # Replace Unicode dashes
   df <- read.csv(textConnection(csv_text),sep = ";")
-  print("Data downloaded.")
+  cat("done.\n")
   return(df)
 }
 
@@ -71,9 +71,9 @@ data_ppa_piiripunktid <- function() {
 #' @param none
 data_agri_alkoholiregister <- function() {
     require(rvest)
-    print("Downloading data...")
+    cat("Downloading data... ")
     x <- read_xml("https://alkoreg.agri.ee/avaandmed")
-    print("Data downloaded.")
+    cat("done.\n")
 
     # Extract features from all products into vectors
     print("Converting XML into data frame...")
